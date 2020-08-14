456b2024c50d4ca6eed8637d24646365f2fb91fa
germanotroian01 <germanotroian@gmail.com>
2020-08-14 11:18:55 -0300

    criação de código para saudação de acordo com a hora do dia

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,26 @@
 package application;
 
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("bom dia!");
+		Scanner sc = new Scanner(System.in);
+		int x;
+		
+		System.out.println("Digite uma hora:");
+		x = sc.nextInt();
+		if (x < 12) {
+			System.out.println("Bom dia");
+		}
+		else if(x < 18) {
+			System.out.println("Boa tarde");
+		}
+		else {
+			System.out.println("Boa noite");
+		}
+		
+		sc.close();
 	}
 
 }
